db0511b13bbcdc28a0d2cd36a00c63cd7b6dd3f3
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-03-14 15:46:03 +0000

    branching trial

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h> /* va_list */
+#include <stdlib.h> /* malloc, free */
+#include <unistd.h> /* write */
+
+char *print_s(va_list list);
+char *print_c(va_list list);
+
+#endif
